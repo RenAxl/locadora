77a5b58ce13eebefd7e9ecbb84e4e83af7809032
Renan Duarte <renandg.axl@gmail.com>
2025-11-06 16:03:58 -0300

    chore: initial project setup

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo PHP</title>
+</head>
+<body>
+    <h1>
+        <?php
+            echo "Projeto Locadora Iniciado";
+        ?>
+    </h1>
+</body>
+</html>
+
