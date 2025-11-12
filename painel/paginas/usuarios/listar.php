c3ffa2713865eb95440a6a51f4008686ffddd355
Renan Duarte <renandg.axl@gmail.com>
2025-11-12 14:31:33 -0300

    complete user table layout

--- painel/paginas/usuarios/listar.php
+++ painel/paginas/usuarios/listar.php
@@ -14,15 +14,28 @@ echo <<<HTML
       </tr>
     </thead>
     <tbody>
-      <!-- Exemplo de linha estática só para ver a tabela renderizar -->
       <tr>
-        <td>Exemplo</td>
+        <td>
+        <input type="checkbox"  class="form-check-input">
+        Exemplo
+        </td>
         <td class="esc">(31) 99999-9999</td>
         <td class="esc">exemplo@teste.com</td>
         <td class="esc">Admin</td>
-        <td class="esc">-</td>
-        <td>-</td>
-      </tr>
+        <td class="esc"><img src="images/perfil/sem-foto.jpg" width="25px"></td>
+        <td>
+	          <big><a href="#" title="Editar Dados"><i class="fa fa-edit text-primary"></i></a></big>
+
+		        <a href="#" title="Excluir usuário" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><big><i class="fa fa-trash-o text-danger"></i></big></a>
+
+            <big><a href="#" title="Mostrar Dados"><i class="fa fa-info-circle text-primary"></i></a></big>
+            
+            <big><a href="#" title="Ativar usuário"><i class="fa fa-check-square"></i></a></big>
+
+            <big><a class="" href="#" title="Dar Permissões"><i class="fa fa-lock text-primary"></i></a></big>
+
+        </td>      
+</tr>
     </tbody>
   </table>
 </small>
