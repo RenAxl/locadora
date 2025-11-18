e612facfafa9e462aee809fe4e8dddc50702b43e
Renan Duarte Gomes <renandg.axl@gmail.com>
2025-11-18 12:19:15 -0300

    List users for testing

--- conexao.php
+++ conexao.php
@@ -16,11 +16,46 @@ $senha = '';
 
 try {
 	$pdo = new PDO("mysql:dbname=$banco;host=$servidor;charset=utf8", "$usuario", "$senha");
-	echo 'Conexão com o banco de dados realizada com sucesso! AGORA OS COMMITs ESTÃO SENDO REALIZADOS NO LINUX UBUNTU';
 } catch (Exception $e) {
 	echo 'Erro ao conectar ao banco de dados!<br>';
 	echo $e;
 }
 
+//variaveis globais
+$nome_sistema = 'Nome Sistema';
+$email_sistema = 'contato@hugocursos.com.br';
+$telefone_sistema = '(31)97527-5084';
+
+$query = $pdo->query("SELECT * from usuarios order by id desc");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$linhas = @count($res);
+
+if($linhas > 0){
+	for($i=0; $i<$linhas; $i++){
+		$id = $res[$i]['id'];
+		$nome = $res[$i]['nome'];
+		$telefone = $res[$i]['telefone'];
+		$email = $res[$i]['email'];
+		$senha = $res[$i]['senha'];
+		$foto = $res[$i]['foto'];
+		$nivel = $res[$i]['nivel'];
+		$endereco = $res[$i]['endereco'];
+		$ativo = $res[$i]['ativo'];
+		$data = $res[$i]['data'];
+
+		echo "ID: $id <br>";
+		echo "Nome: $nome <br>";
+		echo "Telefone: $telefone <br>";
+		echo "Email: $email <br>";
+		echo "Senha: $senha <br>";
+		echo "Foto: $foto <br>";
+		echo "Nível: $nivel <br>";
+		echo "Endereço: $endereco <br>";
+		echo "Ativo: $ativo <br>";
+		echo "Data: $data <br>";
+		echo "<hr>"; 
+
+	}
+}
 
 ?>
