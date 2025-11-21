9dc5952cc69abee66a20c7ab54835c3d60bbd5a6
Renan Duarte Gomes <renandg.axl@gmail.com>
2025-11-21 15:37:22 -0300

    Insert users

--- painel/js/ajax.js
+++ painel/js/ajax.js
@@ -41,3 +41,40 @@ function inserir(){
     $('#modalForm').modal('show');
     limparCampos();
 }
+
+$("#form").submit(function () {
+
+    event.preventDefault();
+    var formData = new FormData(this);
+
+    $.ajax({
+        url: 'paginas/' + pag + "/salvar.php",
+        type: 'POST',
+        data: formData,
+
+        success: function (mensagem) {
+            $('#mensagem').text('');
+            $('#mensagem').removeClass()
+            if (mensagem.trim() == "Salvo com Sucesso") {
+
+                $('#form').trigger("reset");
+
+                $('#btn-fechar').click();
+                listar();          
+
+            } else {
+
+                $('#mensagem').addClass('text-danger')
+                $('#mensagem').text(mensagem)
+            }
+
+
+        },
+
+        cache: false,
+        contentType: false,
+        processData: false,
+
+    });
+
+});
