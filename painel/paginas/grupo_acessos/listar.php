f7ec513d862d9d2cf82356095f2661ab6aa17211
Renan Duarte Gomes <renandg.axl@gmail.com>
2025-11-24 13:43:35 -0300

    List access groups

--- painel/paginas/grupo_acessos/listar.php
+++ painel/paginas/grupo_acessos/listar.php
@@ -2,6 +2,10 @@
 $tabela = 'grupo_acessos';
 require_once("../../../conexao.php");
 
+$query = $pdo->query("SELECT * from $tabela order by id desc");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$linhas = @count($res);
+if($linhas > 0){
 echo <<<HTML
 <small>
 	<table class="table table-hover" id="tabela">
@@ -13,43 +17,42 @@ echo <<<HTML
 	</tr> 
 	</thead> 
 	<tbody>	
-<tr>
-	<td>Cadastros</td>
-	<td>3</td>
-	<td>
-		<big><a href="#" title="Editar"><i class="fa fa-edit text-primary"></i></a></big>
-		<big><a href="#" title="Excluir"><i class="fa fa-trash-o text-danger"></i></a></big>
-	</td>
-</tr>
-
-<tr>
-	<td>Pessoas</td>
-	<td>1</td>
-	<td>
-		<big><a href="#" title="Editar"><i class="fa fa-edit text-primary"></i></a></big>
-		<big><a href="#" title="Excluir"><i class="fa fa-trash-o text-danger"></i></a></big>
-	</td>
-</tr>
-
-<tr>
-	<td>Cadastros</td>
-	<td>3</td>
-	<td>
-		<big><a href="#" title="Editar"><i class="fa fa-edit text-primary"></i></a></big>
-		<big><a href="#" title="Excluir"><i class="fa fa-trash-o text-danger"></i></a></big>
-	</td>
-</tr>
-
-<tr>
-	<td>Pessoas</td>
-	<td>4</td>
-	<td>
-		<big><a href="#" title="Editar"><i class="fa fa-edit text-primary"></i></a></big>
-		<big><a href="#" title="Excluir"><i class="fa fa-trash-o text-danger"></i></a></big>
-	</td>
-</tr>
-
 HTML;
 
+
+for($i=0; $i<$linhas; $i++){
+	$id = $res[$i]['id'];
+	$nome = $res[$i]['nome'];
+    $total_acessos = 'N/A';
+		
+echo <<<HTML
+<tr>
+<td>
+<input type="checkbox" id="seletor-{$id}" class="form-check-input" onchange="selecionar('{$id}')">
+{$nome}
+</td>
+<td class="esc">{$total_acessos}</td>
+
+<td>
+	<big><a href="#" title="Editar"><i class="fa fa-edit text-primary"></i></a></big>
+
+		<a href="#" title="Excluir" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><big><i class="fa fa-trash-o text-danger"></i></big></a>
+
+</td>
+</tr>
+HTML;
+
+}
+
+
+echo <<<HTML
+</tbody>
+<small><div align="center" id="mensagem-excluir"></div></small>
+</table>
+HTML;
+
+}else{
+	echo '<small>Nenhum Registro Encontrado!</small>';
+}
 
 ?>
