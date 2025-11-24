2c10a3ae4fe00f23460044db5c7b635422555a8a
Renan Duarte Gomes <renandg.axl@gmail.com>
2025-11-24 15:27:13 -0300

    Delete access group.

--- painel/paginas/grupo_acessos/listar.php
+++ painel/paginas/grupo_acessos/listar.php
@@ -34,9 +34,19 @@ echo <<<HTML
 <td class="esc">{$total_acessos}</td>
 
 <td>
-	<big><a href="#" onclick="editar('{$id}','{$nome}')" title="Editar Dados"><i class="fa fa-edit text-primary"></i></a></big>
+	<big><a href="#" onclick="editar('{$id}','{$nome}')" title="Editar"><i class="fa fa-edit text-primary"></i></a></big>
 
+	<li class="dropdown head-dpdn2" style="display: inline-block;">
 		<a href="#" title="Excluir" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><big><i class="fa fa-trash-o text-danger"></i></big></a>
+
+		<ul class="dropdown-menu" style="margin-left:-230px;">
+            <li>
+                <div class="notification_desc2">
+                    <p>Confirmar Exclusão? <a href="#" onclick="excluir('{$id}')"><span class="text-danger">Sim</span></a></p>
+                </div>
+            </li>										
+		</ul>
+    </li>	
 
 </td>
 </tr>
@@ -78,5 +88,5 @@ HTML;
     
     	$('#modalForm').modal('show');
 	}
-    
+
 </script>
