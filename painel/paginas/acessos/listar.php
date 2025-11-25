8f9b7b40a34de3ac77c1b403757ed949d6d7a922
Renan Duarte Gomes <renandg.axl@gmail.com>
2025-11-25 12:07:18 -0300

    Using DataTables in the access table (Pagination, Search, number of access per page, and footer information).

--- painel/paginas/acessos/listar.php
+++ painel/paginas/acessos/listar.php
@@ -69,3 +69,17 @@ HTML;
 }
 
 ?>
+
+<script type="text/javascript">
+
+	$(document).ready( function () {		
+    $('#tabela').DataTable({
+    	"language" : {
+            //"url" : '//cdn.datatables.net/plug-ins/1.13.2/i18n/pt-BR.json'
+        },
+        "ordering": false,
+		"stateSave": true
+    });
+} );
+
+</script>
