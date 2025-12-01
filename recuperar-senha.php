e73f077debf249d07418a9a084a14cc1fae68008
Renan Duarte Gomes <renandg.axl@gmail.com>
2025-12-01 14:32:57 -0300

    Recover password

---
+++ recuperar-senha.php
@@ -0,0 +1,39 @@
+<?php 
+require_once("conexao.php");
+
+$email = $_POST['email'];
+
+$query = $pdo->query("SELECT * from usuarios where email = '$email'");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$total_reg = @count($res);
+if($total_reg > 0){    
+	$telefone = $res[0]['telefone'];
+
+        $token = hash('sha256',time());
+
+        $q = $pdo->prepare("UPDATE usuarios SET token=? WHERE email=?");
+        $q->execute([$token,$email]);
+        
+        $reset_link = $url_sistema.'resetar-senha.php'.'?email='.$email.'&token='.$token;
+
+
+    //envio do email
+    $destinatario = $email;
+    $assunto = $nome_sistema . ' - Recuperação de Senha';
+    $mensagem = 'Clique no Link ao lado para atualizar sua senha:' .$reset_link;
+    $cabecalhos = "From: ".$email_sistema;
+   
+    // COMO É SERVIDOR LOCAL, NÃO SERA ENVIADO PARA O E-MAIL.
+    @mail($destinatario, $assunto, $mensagem, $cabecalhos);
+
+    /*PARA VERIFICAR SE O LINK FOI CRIADO E ESTA FUNCIONANDO, 
+    DESCOMENTE O CÓDIGO ABAIXO. ACESSE O LINK, E ALTERE A SENHA.*/
+    //echo $reset_link;
+    //exit();
+
+    echo 'Recuperado com Sucesso';
+}else{
+    echo 'Esse email não está Cadastrado!';
+}
+
+?>
